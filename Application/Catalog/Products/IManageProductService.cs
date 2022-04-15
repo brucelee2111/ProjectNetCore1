ce77de446f42542872a7434dfb0045de8f93d09a
brucelee2111 <brucelee21112000@gmail.com>
2022-04-15 15:05:25 +0700

    update product services

---
+++ Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,28 @@
+﻿using Application.Catalog.Products.Dtos;
+using Application.Catalog.Products.Dtos.Magane;
+using Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        Task<int> Create(ProductCreateRequest request);
+
+        Task<int> Update(ProductUpdateRequest request);
+
+        Task<int> Delete(int productId);
+
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+        Task AddViewcount(int productId);
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+
+        Task<List<ProductViewModel>> GetAll();
+        Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
+
+
+    }
+}
