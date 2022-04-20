21112a8fab431fff3911ffa72d025541eaef9c79
brucelee2111 <brucelee21112000@gmail.com>
2022-04-20 21:29:18 +0700

    add 1

--- AdminApp/Controllers/UserController.cs
+++ AdminApp/Controllers/UserController.cs
@@ -1,8 +1,14 @@
 ﻿using AdminApp.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using ViewModels.System.Users;
 
@@ -11,10 +17,12 @@ namespace AdminApp.Controllers
     public class UserController : Controller
     {
         private readonly IUserApiClient _userApiClient;
+        private readonly IConfiguration _configuration;
 
-        public UserController(IUserApiClient userApiClient)
+        public UserController(IUserApiClient userApiClient, IConfiguration configuration)
         {
             _userApiClient = userApiClient;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
@@ -36,7 +44,26 @@ namespace AdminApp.Controllers
                 return View(ModelState);
             }
             var token = await _userApiClient.Authenticate(request);
+
             return View(token);
+        }
+
+        private ClaimsPrincipal ValidateToken(string jwtToken)
+        {
+            IdentityModelEventSource.ShowPII = true;
+
+            SecurityToken validatedToken;
+            TokenValidationParameters validationParameters = new TokenValidationParameters();
+
+            validationParameters.ValidateLifetime = true;
+
+            validationParameters.ValidAudience = _configuration["Tokens:Issuer"];
+            validationParameters.ValidIssuer = _configuration["Tokens:Issuer"];
+            validationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
+
+            ClaimsPrincipal principal = new JwtSecurityTokenHandler().ValidateToken(jwtToken, validationParameters, out validatedToken);
+
+            return principal;
         }
     }
 }
